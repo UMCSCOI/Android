75cc2a445d27d48256aff0af30c13d77cee95031
seok <rudtjr1206@naver.com>
2026-01-05 15:15:18 +0900

    feat - Navigation 세팅 및 버튼 컴포넌트 추가

---
+++ app/src/main/java/com/stable/scoi/presentation/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.stable.scoi.presentation
+
+import androidx.activity.viewModels
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.stable.scoi.R
+import com.stable.scoi.databinding.ActivityMainBinding
+import com.stable.scoi.presentation.base.BaseActivity
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class MainActivity : BaseActivity<ActivityMainBinding, MainActivityUiState, MainActivityEvent, MainActivityViewModel>(
+    ActivityMainBinding::inflate
+) {
+    override val viewModel: MainActivityViewModel by viewModels()
+    private lateinit var navController: NavController
+
+    override fun initView() {
+        binding.apply {
+            vm = viewModel
+            initNavigation()
+        }
+    }
+
+    override fun initState() {
+        repeatOnStarted {
+            launch {
+                viewModel.uiEvent.collect{
+                    // TODO 이벤트 처리
+                }
+            }
+
+            launch {
+                viewModel.uiState.collect {
+                    // TODO 상태 관리
+                }
+            }
+        }
+    }
+
+    private fun initNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
+        navController = navHostFragment.navController
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            //changeBottomNavigationView(destination.id)
+            //TODO navController.navigate(R.id.calendarFragment) 이런식으로 navigation 이동
+        }
+    }
+}
---
+++ app/src/main/java/com/stable/scoi/presentation/MainActivityViewModel.kt
@@ -0,0 +1,32 @@
+package com.stable.scoi.presentation
+
+import com.stable.scoi.presentation.base.BaseViewModel
+import com.stable.scoi.presentation.base.UiEvent
+import com.stable.scoi.presentation.base.UiState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MainActivityViewModel @Inject constructor() : BaseViewModel<MainActivityUiState, MainActivityEvent>(
+    MainActivityUiState()
+) {
+    fun dummyFun() {
+        updateState {
+            copy(
+                dummy = "~~~"
+            )
+        }
+    }
+
+    fun emitFun() {
+        emitEvent(MainActivityEvent.DummyEvent)
+    }
+}
+
+data class MainActivityUiState(
+    val dummy: String = ""
+) : UiState
+
+sealed class MainActivityEvent : UiEvent {
+    object DummyEvent: MainActivityEvent()
+}
